0f67460926bf0ad141c77f473069744190d71dfd
durkdotdev <durksteed13@gmail.com>
2023-01-24 10:51:47 -0500

    Add TSDocs

--- packages/aria-typed/src/index.ts
+++ packages/aria-typed/src/index.ts
@@ -2,6 +2,11 @@ import { AriaAttributes } from "./attributes";
 import { AriaRole } from "./roles";
 
 export type AriaTypes = AriaAttributes & {
+  /**
+   * ARIA roles provide semantic meaning to content, allowing screen readers and other tools to present and support interaction with object in a way that is consistent with user expectations of that type of object.
+   *
+   * ARIA roles can be used to describe elements that don't natively exist in HTML or exist but don't yet have full browser support.
+   */
   role?: AriaRole;
 };
 
