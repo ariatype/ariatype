3d3dc6bef47bc840f524f8bd0213c0181ebdd733
ddddurk <ddddurk@proton.me>
2023-04-12 14:07:58 -0400

    chore(@dddstack/ariatype-aria-attributes-drag-and-drop): create package

---
+++ packages/ariatype-aria-attributes-drag-and-drop/src/ariaAttributesDragAndDrop.ts
@@ -0,0 +1,6 @@
+import type { AriaAttributeDragAndDrop } from "./ariaAttributesDragAndDrop.types";
+
+export const ariaAttributesDragAndDrop = [
+  "aria-dropeffect",
+  "aria-grabbed"
+] as AriaAttributeDragAndDrop[];
---
+++ packages/ariatype-aria-attributes-drag-and-drop/src/index.ts
@@ -0,0 +1,3 @@
+export * from "./ariaAttributesDragAndDrop";
+
+export * from "./ariaAttributesDragAndDrop.types";
