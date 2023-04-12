8d354dffd7734af64209735ff2120e12c9baddb5
ddddurk <ddddurk@proton.me>
2023-04-12 16:39:24 -0400

    chore(@dddstack/ariatype-aria-roles-landmark): create package

---
+++ packages/ariatype-aria-roles-landmark/src/ariaRolesLandmark.types.ts
@@ -0,0 +1,5 @@
+import type { AriaRolesGeneric } from "@dddstack/ariatype-aria-roles-generic";
+
+import type { AriaRoleLandmark } from "./ariaRoleLandmark.types";
+
+export type AriaRolesLandmark = AriaRolesGeneric<AriaRoleLandmark>;
