79aaac8f789fdcfd2d09236702054c6ec699df1b
Durk Steed Jr <139193744+durksteedjr@users.noreply.github.com>
2024-10-10 14:11:02 +0200

    chore: v1 cleanup

--- @ariatype/ariatype/src/types/partiallyRequiredAriaTypes.ts
+++ @ariatype/ariatype/src/types/partiallyRequiredAriaTypes.ts
@@ -1,4 +1,4 @@
-import type { AriaType } from "./ariaType.types";
+import type { AriaType } from "./ariaTypes.types";
 import type { PartiallyRequiredProps } from "@ariatype/partially-required-props/dist";
 
 export type PartiallyRequiredAriaType<T extends keyof AriaType> =
